29240f8c58fd10928d21dbdcfad15b49f2f86d31
Frédéric Côté <fredcote-_-@hotmail.com>
2022-04-26 17:18:36 -0400

    lint and spawn

--- js/StdEnv.js
+++ js/StdEnv.js
@@ -56,189 +56,215 @@ Other usages should be like:
 
 class StdEnv {
     constructor() {
-            this.init = function(terrainPath, avatarPath, x, y, z) {
-                    this.graphicTier = localProxy.tier !== undefined ? localProxy.tier : 0;
+        this.scene = new THREE.Scene();
+        this.init = function(terrainPath, avatarPath, x, y, z) {
+            this.graphicTier = localProxy.tier !== undefined ? localProxy.tier : 0;
 
-                    // ===== renderer =====
-                    this.renderer = new THREE.WebGLRenderer({ alpha: true });
-                    this.renderer.setPixelRatio(1);
-                    this.renderer.setSize(window.innerWidth, window.innerHeight);
-                    this.renderer.shadowMap.enabled = true;
-                    this.renderer.shadowMap.type = THREE.VSMShadowMap;
-                    document.body.appendChild(this.renderer.domElement);
+            // ===== renderer =====
+            this.renderer = new THREE.WebGLRenderer({ alpha: true });
+            this.renderer.setPixelRatio(1);
+            this.renderer.setSize(window.innerWidth, window.innerHeight);
+            this.renderer.shadowMap.enabled = true;
+            this.renderer.shadowMap.type = THREE.VSMShadowMap;
+            document.body.appendChild(this.renderer.domElement);
 
-                    // ===== scene =====
-                    this.scene = new THREE.Scene();
-                    const scene = this.scene;
-                    this.scene.background = new THREE.Color(0x69e6f4);
-                    this.scene.fog = new THREE.Fog(0x69e6f4, 1600, 2000);
+            // ===== scene =====
+            const scene = this.scene;
+            this.scene.background = new THREE.Color(0x69e6f4);
+            this.scene.fog = new THREE.Fog(0x69e6f4, 1600, 2000);
 
-                    // ===== camera =====
-                    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
-                    this.camera.position.y = 1.6;
-                    this.camera.lookAt(0, 1.7, -1);
-                    this.dummyCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
-                    this.dummyCamera.position.y = 1.6;
-                    this.dummyCamera.lookAt(0, 1.7, -1);
+            // ===== camera =====
+            this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
+            this.camera.position.y = 1.6;
+            this.camera.lookAt(0, 1.7, -1);
+            this.dummyCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
+            this.dummyCamera.position.y = 1.6;
+            this.dummyCamera.lookAt(0, 1.7, -1);
 
-                    // ===== lights =====
-                    const light = new THREE.HemisphereLight(0xffeeff, 0x777788, 1);
-                    light.position.set(0.5, 1, 0.75);
-                    this.scene.add(light);
+            // ===== lights =====
+            const light = new THREE.HemisphereLight(0xffeeff, 0x777788, 1);
+            light.position.set(0.5, 1, 0.75);
+            this.scene.add(light);
 
-                    if (this.graphicTier !== LOW) {
-                        shadowLight = new DefaultDirectionalLight(this.graphicTier);
-                        this.scene.add(shadowLight);
-                        this.scene.add(shadowLight.target);
+            if (this.graphicTier !== LOW) {
+                shadowLight = new DefaultDirectionalLight(this.graphicTier);
+                this.scene.add(shadowLight);
+                this.scene.add(shadowLight.target);
+            }
+
+            // ===== shaders =====
+            this.composer = new DefaultComposer(this.renderer, this.scene, this.camera);
+
+            // ===== load the terrain =====
+            function onProgress(xr) { /* console.log((xr.loaded / xr.total) * 100)*/ }
+
+            function onError(e) { console.log(e) };
+
+            const loader = new GLTFLoader();
+            loader.load(terrainPath, (object) => {
+                object.scene.position.set(x, y, z);
+                object.scene.traverse(object => {
+                    if (object.isMesh) {
+                        object.castShadow = true;
+                        object.receiveShadow = true;
+                        object.material.roughness = 1;
+                        if (object.material.map) {
+                            object.material.map.anisotropy = 16;
+                            object.material.map.needsUpdate = true;
+                        }
+                        const cloned = new THREE.Mesh(object.geometry, object.material);
+                        object.getWorldPosition(cloned.position);
+                        //object.updateMatrixWorld();
+                        if (object.material.emissive && (object.material.emissive.r > 0 || object.material.emissive.g > 0 || object.material.color.b > 0)) {
+                            /* object.updateMatrixWorld();
+                                cloned.matrix.copy(object.matrixWorld);*/
+                            bloomScene.attach(cloned);
+                        }
                     }
-
-                    // ===== shaders =====
-                    this.composer = new DefaultComposer(this.renderer, this.scene, this.camera);
-
-                    // ===== load the terrain =====
-                    function onProgress(xr) { /* console.log((xr.loaded / xr.total) * 100)*/ }
-
-                    function onError(e) { console.log(e) };
-
-                    const loader = new GLTFLoader();
-                    loader.load(terrainPath, (object) => {
-                        object.scene.position.set(x, y, z);
-                        object.scene.traverse(object => {
-                            if (object.isMesh) {
-                                object.castShadow = true;
-                                object.receiveShadow = true;
-                                object.material.roughness = 1;
-                                if (object.material.map) {
-                                    object.material.map.anisotropy = 16;
-                                    object.material.map.needsUpdate = true;
-                                }
-                                const cloned = new THREE.Mesh(object.geometry, object.material);
-                                object.getWorldPosition(cloned.position);
-                                //object.updateMatrixWorld();
-                                if (object.material.emissive && (object.material.emissive.r > 0 || object.material.emissive.g > 0 || object.material.color.b > 0)) {
-                                    /* object.updateMatrixWorld();
-                                     cloned.matrix.copy(object.matrixWorld);*/
-                                    bloomScene.attach(cloned);
-                                }
-                            }
-                            if (object.isLight) {
-                                object.parent.remove(object);
-                            }
-                        });
-                        scene.add(object.scene);
-                        let geometries = [];
-                        object.scene.traverse(object => {
-                            if (object.geometry && object.visible) {
-                                const cloned = object.geometry.clone();
-                                cloned.applyMatrix4(object.matrixWorld);
-                                for (const key in cloned.attributes) {
-                                    if (key !== 'position') { cloned.deleteAttribute(key); }
-                                }
-                                geometries.push(cloned);
-                            }
-                        });
-                        const mergedGeometry = BufferGeometryUtils.mergeBufferGeometries(geometries, false);
-                        mergedGeometry.boundsTree = new MeshBVH(mergedGeometry, { lazyGeneration: false });
-                        collider = new THREE.Mesh(mergedGeometry);
-                        collider.material.wireframe = true;
-                        collider.material.opacity = 0.5;
-                        collider.material.transparent = true;
-                        collider.visible = false;
-                        scene.add(collider);
-
-                        const visualizer = new MeshBVHVisualizer(collider, 10);
-                        visualizer.visible = false;
-                        visualizer.update();
-                        scene.add(visualizer);
-                        loader.load('glb/animation.glb', (gltf) => {
-                            loader.load(avatarPath, (vanguard) => {
-                                vanguard.scene.scale.set(0.2, 0.2, 0.2);
-                                this.playerAvatar = new PlayerAvatar(2.5, 30, vanguard.scene, {
-                                    "idle": gltf.animations[2],
-                                    "walk": gltf.animations[1],
-                                    "run": gltf.animations[3],
-                                    "jump": gltf.animations[4],
-                                    "fall": gltf.animations[5]
-                                }, {
-                                    scene
-                                });
-                            });
-                        });
-                    }, onProgress, onError);
-                    this.entities = [];
-                    // ===== player =====
-                    this.player = new Player();
-                    window.player = this.player;
-                    this.scene.add(this.player);
-
-                    // ===== controls =====
-                    this.controls = new PointerLockControls(this.dummyCamera, document.body);
-                    this.controls.sensitivityY = 0.002;
-                    this.scene.add(this.controls.getObject());
-
-                    document.addEventListener('keydown', (event) => {
-                        if (event.key === "v") {
-                            if (this.targetControlVector === this.thirdPersonControls) {
-                                this.targetControlVector = this.fpsControls;
-                                this.controls.sensitivityY = -0.002;
-                            } else {
-                                this.targetControlVector = this.thirdPersonControls;
-                                this.controls.sensitivityY = 0.002;
-                            }
+                    if (object.isLight) {
+                        object.parent.remove(object);
+                    }
+                });
+                scene.add(object.scene);
+                let geometries = [];
+                object.scene.traverse(object => {
+                    if (object.geometry && object.visible) {
+                        const cloned = object.geometry.clone();
+                        cloned.applyMatrix4(object.matrixWorld);
+                        for (const key in cloned.attributes) {
+                            if (key !== 'position') { cloned.deleteAttribute(key); }
                         }
-                        this.player.keys[event.key] = true;
+                        geometries.push(cloned);
+                    }
+                });
+                const mergedGeometry = BufferGeometryUtils.mergeBufferGeometries(geometries, false);
+                mergedGeometry.boundsTree = new MeshBVH(mergedGeometry, { lazyGeneration: false });
+                collider = new THREE.Mesh(mergedGeometry);
+                collider.material.wireframe = true;
+                collider.material.opacity = 0.5;
+                collider.material.transparent = true;
+                collider.visible = false;
+                scene.add(collider);
+
+                const visualizer = new MeshBVHVisualizer(collider, 10);
+                visualizer.visible = false;
+                visualizer.update();
+                scene.add(visualizer);
+                loader.load('glb/animation.glb', (gltf) => {
+                    loader.load(avatarPath, (vanguard) => {
+                        vanguard.scene.scale.set(0.2, 0.2, 0.2);
+                        this.playerAvatar = new PlayerAvatar(2.5, 30, vanguard.scene, {
+                            "idle": gltf.animations[2],
+                            "walk": gltf.animations[1],
+                            "run": gltf.animations[3],
+                            "jump": gltf.animations[4],
+                            "fall": gltf.animations[5]
+                        }, {
+                            scene
+                        });
                     });
-                    document.addEventListener('keyup', (event) => {
-                        this.player.keys[event.key] = false;
-                    });
+                });
+            }, onProgress, onError);
+            this.entities = [];
+            // ===== player =====
+            this.player = new Player();
+            window.player = this.player;
+            this.scene.add(this.player);
 
-                    window.addEventListener('keydown', (e) => {
-                        if (e.keyCode === 32 && e.target === document.body) {
-                            e.preventDefault();
-                        }
-                    });
+            // ===== controls =====
+            this.controls = new PointerLockControls(this.dummyCamera, document.body);
+            this.controls.sensitivityY = 0.002;
+            this.scene.add(this.controls.getObject());
 
-                    // ===== graphic settings =====
-                    this.setGraphicsSetting(this.graphicTier);
-
-                } // -- end init
-
-            this.setShadowLightTier = function(graphicTier) {
-                if (shadowLight) {
-                    this.scene.remove(shadowLight.target);
-                    this.scene.remove(shadowLight);
-                    shadowLight.dispose();
-                    shadowLight.shadow.dispose();
+            document.addEventListener('keydown', (event) => {
+                if (event.key === "v") {
+                    if (this.targetControlVector === this.thirdPersonControls) {
+                        this.targetControlVector = this.fpsControls;
+                        this.controls.sensitivityY = -0.002;
+                    } else {
+                        this.targetControlVector = this.thirdPersonControls;
+                        this.controls.sensitivityY = 0.002;
+                    }
                 }
+                this.player.keys[event.key] = true;
+            });
+            document.addEventListener('keyup', (event) => {
+                this.player.keys[event.key] = false;
+            });
 
-                if (graphicTier !== LOW) {
-                    shadowLight = new DefaultDirectionalLight(this.graphicTier);
-                    this.scene.add(shadowLight);
-                    this.scene.add(shadowLight.target);
-                } else {
-                    shadowLight = undefined;
+            window.addEventListener('keydown', (e) => {
+                if (e.keyCode === 32 && e.target === document.body) {
+                    e.preventDefault();
                 }
+            });
+
+            // ===== graphic settings =====
+            this.setGraphicsSetting(this.graphicTier);
+
+        } // -- end init
+
+        this.setShadowLightTier = function(graphicTier) {
+            if (shadowLight) {
+                this.scene.remove(shadowLight.target);
+                this.scene.remove(shadowLight);
+                shadowLight.dispose();
+                shadowLight.shadow.dispose();
             }
 
-            this.setGraphicsSetting = function(graphicTier) {
-                this.graphicTier = graphicTier;
-                this.setShadowLightTier(graphicTier);
-                this.composer.setGraphicsSetting(graphicTier, this.renderer, this.scene);
+            if (graphicTier !== LOW) {
+                shadowLight = new DefaultDirectionalLight(this.graphicTier);
+                this.scene.add(shadowLight);
+                this.scene.add(shadowLight.target);
+            } else {
+                shadowLight = undefined;
             }
+        }
 
-            this.increaseGraphicSettings = function() {
-                this.graphicTier += 1;
-                this.graphicTier %= 4;
-                localProxy.tier = this.graphicTier;
-                this.setGraphicsSetting(this.graphicTier)
-            }
-            this.cameraPosition = new THREE.Vector3();
-            this.cameraTarget = new THREE.Vector3();
-            this.fpsControls = new THREE.Vector4(0.01, Math.PI - 0.01, 0.01, 1);
-            this.thirdPersonControls = new THREE.Vector4(Math.PI / 3, Math.PI / 2 - 0.01, 40, 0.2);
-            this.controlVector = this.thirdPersonControls.clone();
-            this.targetControlVector = this.thirdPersonControls;
-        } // -- end constructor
+        this.setGraphicsSetting = function(graphicTier) {
+            this.graphicTier = graphicTier;
+            this.setShadowLightTier(graphicTier);
+            this.composer.setGraphicsSetting(graphicTier, this.renderer, this.scene);
+        }
+
+        this.increaseGraphicSettings = function() {
+            this.graphicTier += 1;
+            this.graphicTier %= 4;
+            localProxy.tier = this.graphicTier;
+            this.setGraphicsSetting(this.graphicTier)
+        }
+
+        this.spawnOtherPlayer = function(avatarPath){
+            console.log(this.scene);
+            const scene = this.scene;
+            const loader = new GLTFLoader();
+            loader.load('glb/animation.glb', (gltf) => {
+                loader.load(avatarPath, (vanguard) => {
+                    vanguard.scene.scale.set(0.2, 0.2, 0.2);
+                    this.otherPlayer = new PlayerAvatar(2.5, 30, vanguard.scene, {
+                        "idle": gltf.animations[2],
+                        "walk": gltf.animations[1],
+                        "run": gltf.animations[3],
+                        "jump": gltf.animations[4],
+                        "fall": gltf.animations[5]
+                    }, {
+                        scene
+                    });
+                });
+            });
+            console.log(this.otherPlayer)
+        }
+
+        this.getLocation = () => {
+            return this.playerAvatar.position;
+        }
+
+        this.cameraPosition = new THREE.Vector3();
+        this.cameraTarget = new THREE.Vector3();
+        this.fpsControls = new THREE.Vector4(0.01, Math.PI - 0.01, 0.01, 1);
+        this.thirdPersonControls = new THREE.Vector4(Math.PI / 3, Math.PI / 2 - 0.01, 40, 0.2);
+        this.controlVector = this.thirdPersonControls.clone();
+        this.targetControlVector = this.thirdPersonControls;
+    } // -- end constructor
 
     update() {
         const delta = Math.min(clock.getDelta(), 0.1);

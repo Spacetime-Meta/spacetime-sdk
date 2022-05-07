741e6eed20ff82bc7459d9056dc4dfda64d0fbbe
Frédéric Côté <fredcote-_-@hotmail.com>
2022-05-06 22:13:23 -0400

    Create class PlayerRemote and update UML

---
+++ js/entities/PlayerRemote.js
@@ -0,0 +1,9 @@
+import { CapsuleEntity } from "./CapsuleEntity.js";
+import { Vector3 } from 'https://cdn.skypack.dev/pin/three@v0.137.0-X5O2PK3x44y1WRry67Kr/mode=imports/optimized/three.js';
+
+class PlayerRemote extends CapsuleEntity {
+    constructor() {
+        super(5, 30);
+        
+    }
+}

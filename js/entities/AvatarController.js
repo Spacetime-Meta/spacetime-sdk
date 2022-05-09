747380c754d9872a69b7e5663290ed3f94b7054b
codeeMadness <codee.madness@gmail.com>
2022-05-09 22:04:18 +0700

    #37 Move avatar loading logic inside AvatarController
    Move the loader and the loading logic inside the AvatarController. The constructor for the AvatarController should only require the animationURL, the avatarURL and the scene.

--- js/entities/AvatarController.js
+++ js/entities/AvatarController.js
@@ -1,5 +1,6 @@
 import * as THREE from 'https://cdn.skypack.dev/pin/three@v0.137.0-X5O2PK3x44y1WRry67Kr/mode=imports/optimized/three.js';
 import * as SkeletonUtils from 'https://cdn.skypack.dev/three@0.136.0/examples/jsm/utils/SkeletonUtils.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.136.0/examples/jsm/loaders/GLTFLoader.js';
 
 function angleDifference(angle1, angle2) {
     const diff = ((angle2 - angle1 + Math.PI) % (Math.PI * 2)) - Math.PI;
@@ -7,45 +8,61 @@ function angleDifference(angle1, angle2) {
 }
 
 class AvatarController extends THREE.Object3D {
-    constructor(radius, size, model, animations, {
-        scene,
-        entities
-    }) {
+    constructor(animationURL, avatarURL, scene) {
         super();
-        this.size = size;
-        this.radius = radius;
-        this.model = SkeletonUtils.clone(model);
-        this.model.traverse(child => {
-            if (child.isMesh) {
-                child.geometry = child.geometry.clone();
-                child.material = new THREE.MeshStandardMaterial({ color: child.material.color, transparent: true, roughness: child.material.roughness, metalness: child.material.metalness, map: child.material.map, normalMap: child.material.normalMap });
-            }
-        })
-        this.model.traverse(child => {
-            if (child.isMesh) {
-                child.castShadow = true;
-                child.receiveShadow = true;
-                child.frustumCulled = false;
-            }
+        const loader = new GLTFLoader();
+        loader.load(animationURL, (gltf) => {
+            
+            loader.load(avatarURL, (vanguard) => {
+                this.radius = 2.5;
+                this.size = 30;
+
+                this.model = vanguard.scene;
+                this.model.scale.set(0.2, 0.2, 0.2);
+                // this.model = SkeletonUtils.clone(this.model);
+                this.model.traverse(child => {
+                    if (child.isMesh) {
+                        child.geometry = child.geometry.clone();
+                        child.material = new THREE.MeshStandardMaterial({ color: child.material.color, transparent: true, roughness: child.material.roughness, metalness: child.material.metalness, map: child.material.map, normalMap: child.material.normalMap });
+                    }
+                })
+                this.model.traverse(child => {
+                    if (child.isMesh) {
+                        child.castShadow = true;
+                        child.receiveShadow = true;
+                        child.frustumCulled = false;
+                    }
+                });
+
+                this.current = "none";
+                this.mixer = new THREE.AnimationMixer(this.model);
+                this.animations = {
+                    "idle": gltf.animations[2],
+                    "walk": gltf.animations[1],
+                    "run": gltf.animations[3],
+                    "jump": gltf.animations[4],
+                    "fall": gltf.animations[5]
+                };
+                Object.entries(this.animations).forEach(([anim, clip]) => {
+                    this.animations[anim] = this.mixer.clipAction(clip);
+                });
+
+                this.lastChange = performance.now() - 250;
+                this.animations.jump.loop = THREE.LoopPingPong;
+                this.scene = scene;
+                this.scene.add(this.model);
+                this.delta = 0;
+                this.box = new THREE.Box3();
+                this.updateBox();
+                this.positionChange = new THREE.Vector3();
+                this.play("idle", 0);
+                this.jumpTick = 0;
+                this.opacity = 1;
+
+            });
         });
-        this.current = "none";
-        this.mixer = new THREE.AnimationMixer(this.model);
-        this.animations = animations;
-        Object.entries(animations).forEach(([anim, clip]) => {
-            this.animations[anim] = this.mixer.clipAction(clip);
-        });
-        this.lastChange = performance.now() - 250;
-        this.animations.jump.loop = THREE.LoopPingPong;
-        this.scene = scene;
-        this.scene.add(this.model);
-        this.delta = 0;
-        this.box = new THREE.Box3();
-        this.updateBox();
-        this.positionChange = new THREE.Vector3();
-        this.play("idle", 0);
-        this.jumpTick = 0;
-        this.opacity = 1;
     }
+
     play(anim, time = 0.5) {
         if (anim === this.current) {
             return;
@@ -65,8 +82,10 @@ class AvatarController extends THREE.Object3D {
             this.animations[this.current].play();
         }
     }
+
     update(delta, frustum, fpsCamera) {
         this.delta = delta;
+        if(typeof this.box == 'undefined') return;
         this.updateBox();
         this.model.position.copy(this.position);
         // this.model.position.y -= (this.size) / 2 - this.radius / 2;
@@ -127,6 +146,7 @@ class AvatarController extends THREE.Object3D {
             }
         })
     }
+
     updateBox() {
         this.box.setFromPoints([
             new THREE.Vector3(this.position.x + this.radius, this.position.y, this.position.z + this.radius),

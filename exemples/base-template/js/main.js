7712eebed23ef6a5761f3c9387e00b925a44af37
Frédéric Côté <fredcote-_-@hotmail.com>
2022-05-13 11:45:57 -0400

    UI logic was moved inside StdEnv

--- exemples/base-template/js/main.js
+++ exemples/base-template/js/main.js
@@ -1,14 +1,5 @@
 import { StdEnv } from '../../../js/StdEnv.js';
 
-let stats;
-
-let settings = ["Low", "Medium", "High", "Ultra"];
-
-const blocker = document.getElementById('blocker');
-const instructions = document.getElementById('instructions');
-const flyout = document.getElementById('fly-out');
-const graphics = document.getElementById("graphics");
-
 const VE = new StdEnv();
 init();
 
@@ -17,34 +8,6 @@ function init() {
     VE.init();
     VE.loadTerrain('glb/base_template.glb', 0, -20, 0)
     VE.spawnPlayer('glb/vanguard.glb')
-
-    // ===== controls =====
-    VE.controls.addEventListener('lock', function() {
-        flyout.innerHTML = 'ESC To Return'
-        instructions.style.display = 'none';
-        blocker.style.display = 'none';
-    });
-
-    VE.controls.addEventListener('unlock', function() {
-        flyout.innerHTML = 'Back To Map'
-        blocker.style.display = 'block';
-        instructions.style.display = '';
-    });
-
-    instructions.addEventListener('click', function() {
-        VE.controls.lock();
-    });
-
-    graphics.innerHTML = "Graphics: " + settings[VE.graphicTier];
-    graphics.addEventListener('click', function() {
-        VE.increaseGraphicSettings();
-        graphics.innerHTML = "Graphics: " + settings[VE.graphicTier];
-    });
-
-    // ===== stats =====
-    // stats = new Stats();
-    // stats.showPanel(0);
-    // document.body.appendChild(stats.dom);
 }
 
 if (!window.requestPostAnimationFrame) {
@@ -59,7 +22,5 @@ function animate() {
     requestPostAnimationFrame(animate);
 
     VE.update();
-
-    // stats.update();
 }
 requestPostAnimationFrame(animate);
--- exemples/spawn-planet/js/main.js
+++ exemples/spawn-planet/js/main.js
@@ -1,14 +1,4 @@
 import { StdEnv } from '../../../js/StdEnv.js';
-
-        let stats;
-
-        let settings = ["Low", "Medium", "High", "Ultra"];
-
-        const blocker = document.getElementById('blocker');
-        const instructions = document.getElementById('instructions');
-        const flyout = document.getElementById('fly-out');
-        const graphics = document.getElementById("graphics");
-
         const VE = new StdEnv();
 
         init();
@@ -18,34 +8,6 @@ import { StdEnv } from '../../../js/StdEnv.js';
             VE.init();
             VE.loadTerrain('glb/spawnplanet.glb', 0, -20, 0)
             VE.spawnPlayer('glb/vanguard.glb')
-
-            // ===== controls =====
-            VE.controls.addEventListener('lock', function() {
-                flyout.innerHTML = 'ESC To Return'
-                instructions.style.display = 'none';
-                blocker.style.display = 'none';
-            });
-
-            VE.controls.addEventListener('unlock', function() {
-                flyout.innerHTML = 'Back To Map'
-                blocker.style.display = 'block';
-                instructions.style.display = '';
-            });
-
-            instructions.addEventListener('click', function() {
-                VE.controls.lock();
-            });
-
-            graphics.innerHTML = "Graphics: " + settings[VE.graphicTier];
-            graphics.addEventListener('click', function() {
-                VE.increaseGraphicSettings();
-                graphics.innerHTML = "Graphics: " + settings[VE.graphicTier];
-            });
-
-            // ===== stats =====
-            // stats = new Stats();
-            // stats.showPanel(0);
-            // document.body.appendChild(stats.dom);
         }
 
         if (!window.requestPostAnimationFrame) {
@@ -60,7 +22,5 @@ import { StdEnv } from '../../../js/StdEnv.js';
             requestPostAnimationFrame(animate);
 
             VE.update();
-
-            // stats.update();
         }
         requestPostAnimationFrame(animate);

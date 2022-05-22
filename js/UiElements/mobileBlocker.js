d163b5cdc3534b1d604ab70c20444e5605265872
Frédéric Côté <fredcote-_-@hotmail.com>
2022-05-22 11:39:17 -0400

    temporary black screen when going on mobile

---
+++ js/UiElements/mobileBlocker.js
@@ -0,0 +1,30 @@
+const mobileBlocker = function() {
+    const mobileBlocker = document.createElement('div');
+    mobileBlocker.id = 'mobileBlocker';
+
+    //======style====
+    var style = 'position: absolute;';
+    style += 'height: 100%;';
+    style += 'width: 100%;';
+    style += 'display: flex;';
+    style += 'color: turquoise;';
+    style += 'background-color: black;';
+    style += 'z-index: 1000;';
+    style += 'flex-direction: row;';
+    style += 'justify-content: center;';
+    style += 'align-items: center;';
+
+    //======attach style====
+    loadingScreen.style.cssText = style;
+
+    //======loading======
+    var mobileBlockerText = document.createElement('div');
+    mobileBlockerText.id = 'mobileBlockerText';
+    mobileBlockerText.innerHTML = 'This app is not available on mobile.';
+
+    //======append loading page====
+    mobileBlocker.appendChild(mobileBlockerText);
+    document.body.appendChild(loadingScreen);
+}
+
+export default mobileBlocker;
